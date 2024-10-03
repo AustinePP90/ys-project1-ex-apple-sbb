fc7bb65a4c2132b0f7cfd0125fcd82595532391c
Austine <chucky891213@naver.com>
2024-10-03 18:34:47 +0900

    MR: Test 코드 작성

---
+++ src/main/java/com/mysite/sbbmybatis/HelloController.java
@@ -0,0 +1,17 @@
+package com.mysite.sbbmybatis;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/message")
+public class HelloController {
+	
+	@GetMapping
+	@ResponseBody
+	public String printMessage() {
+		return "Hello World ^^";
+	}
+}
